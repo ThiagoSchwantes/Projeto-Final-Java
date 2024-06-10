6ad3ee54e92885810762f980fa123f943e16a560
Pedro Henrique Boldori Bier <124646793+Pedrohbbier@users.noreply.github.com>
2024-06-09 22:12:38 -0300

    feat: Employee register

---
+++ SGOS/src/br/edu/up/controllers/ControleDeFuncionarios.java
@@ -0,0 +1,83 @@
+package br.edu.up.controllers;
+
+import java.util.Iterator;
+import java.util.List;
+
+import br.edu.up.models.Cliente;
+import br.edu.up.models.Funcionario;
+import br.edu.up.daos.GerenciadorDeFuncionariosDAO;
+
+public class ControleDeFuncionarios {
+
+    private List<Funcionario> funcionarios;
+    private GerenciadorDeFuncionariosDAO daoFuncionario;
+    private int maiorId;
+
+    public ControleDeFuncionarios() {
+        daoFuncionario = new GerenciadorDeFuncionariosDAO();
+        funcionarios = daoFuncionario.getFuncionarios();
+        maiorId = 0;
+
+        for(Funcionario funcionario : this.funcionarios) {
+            if (funcionario.getFuncionarioId() > maiorId) {
+                maiorId = funcionario.getFuncionarioId();
+            }
+        }
+
+    }
+
+    public int getProximoId() {
+        return ++maiorId;
+    }
+
+    public List<Funcionario> getFuncionarios() {
+        return funcionarios;
+    }
+
+    public void setFuncionarios(List<Funcionario> Funcionarios) {
+        this.funcionarios = Funcionarios;
+    }
+
+     public void adicionarFuncionario(Funcionario funcionario) {
+        funcionarios.add(funcionario);
+        daoFuncionario.gravarArquivo();
+    }
+
+    public void deletarFuncionario(String cpf) {
+        Iterator<Funcionario> iterator = funcionarios.iterator();
+
+        while(iterator.hasNext()){
+            Funcionario funcionario = iterator.next();
+            if(funcionario instanceof Funcionario){
+                Funcionario funcionarioG = (Funcionario) funcionario;
+                if(funcionarioG.getCpf().equals(cpf)){
+                    iterator.remove();
+                    break;
+                }
+            }
+        }
+        daoFuncionario.gravarArquivo();
+
+
+    }
+
+
+    public void alterarFuncionario(String cpf, Funcionario funcionarioAlterado) {
+        for (Funcionario funcionario : funcionarios) {
+            if(funcionario.getCpf().equals(cpf)){
+                funcionario.setNomeFuncionario(funcionarioAlterado.getNomeFuncionario());
+                funcionario.setRg(funcionarioAlterado.getRg());
+                funcionario.setCep(funcionarioAlterado.getCep());
+                funcionario.setEndereco(funcionarioAlterado.getEndereco());
+                funcionario.setBairro(funcionarioAlterado.getBairro());
+                funcionario.setCidade(funcionarioAlterado.getCidade());
+                break;
+            }
+        }
+        daoFuncionario.gravarArquivo();
+    }
+
+
+    
+}
+
---
+++ SGOS/src/br/edu/up/views/menus/cadastos/MenuFuncionario.java
@@ -0,0 +1,198 @@
+package br.edu.up.views.menus.cadastos;
+
+import java.util.List;
+
+import br.edu.up.controllers.ControleDeFuncionarios;
+import br.edu.up.models.Funcionario;
+import br.edu.up.util.Prompt;
+
+public class MenuFuncionario {
+
+
+    ControleDeFuncionarios controleDeFuncionarios = new ControleDeFuncionarios();
+
+    public void mostrar(){
+        Prompt.limparConsole();
+
+        Prompt.separador();
+        Prompt.imprimir("MENU DE FUNCIONÁRIO");
+        Prompt.separador();
+
+        Prompt.imprimir("Digite uma das opções:");
+        Prompt.imprimir("\t1 - Cadastrar funcionário");
+        Prompt.imprimir("\t2 - Listar funcionários");
+        Prompt.imprimir("\t3 - Alterar funcionário");
+        Prompt.imprimir("\t4 - Deletar funcionário");
+        Prompt.imprimir("\t5 - Voltar para o menu de cadastro");
+
+        int opcao1 = Prompt.lerInteiro("Digite aqui: ");
+        boolean sair = false;
+
+        switch (opcao1) {
+            case 1:
+                cadastrarFuncionario();
+                break;
+            case 2:
+                List<Funcionario> funcionarios = controleDeFuncionarios.getFuncionarios();
+                if(funcionarios.isEmpty()){
+                    Prompt.imprimir("Não há funcionários cadastrados.");
+                }else{
+                    for (Funcionario funcionario : funcionarios) {
+                        Prompt.imprimir(funcionario.toStringBasico());
+                    }
+                }
+                break;
+            case 3:
+                String cpfAlterar = getCpf();
+                Funcionario funcionarioAlterado = alterarFuncionario();
+
+                controleDeFuncionarios.alterarFuncionario(cpfAlterar, funcionarioAlterado);
+                break;
+            case 4:
+                String cpfDeletar = getCpf();
+
+                controleDeFuncionarios.deletarFuncionario(cpfDeletar);
+                break;
+            case 5:
+                sair = true;
+                break;
+            default:
+                Prompt.limparConsole();
+                Prompt.imprimir("Valor Inválido.");
+                break;
+        }
+        if(!sair){
+            Prompt.separador();
+            Prompt.pressionarEnter();
+            mostrar();
+        }
+    }
+
+
+    public Funcionario cadastrarFuncionario() {
+        Integer clienteId = controleDeFuncionarios.getProximoId();
+        String nomeCliente = lerNomeFuncionario();
+        String rg = lerRg();
+        String cpf = lerCpf();
+        String cep = lerCep();
+        String endereco = lerEndereco();
+        String bairro = lerBairro();
+        String cidade = lerCidade();
+
+        Funcionario cliente = new Funcionario(clienteId, nomeCliente, rg, cpf, cep, endereco, bairro, cidade);
+        controleDeFuncionarios.adicionarFuncionario(cliente);
+        return cliente;
+    }
+    
+
+    private String lerNomeFuncionario() {
+        while (true) {
+            String nomeFuncionario = Prompt.lerLinha("Informe o nome do funcionário: ");
+            try {
+                Funcionario funcionario = new Funcionario();
+                funcionario.setNomeFuncionario(nomeFuncionario);
+                return nomeFuncionario;
+            } catch (IllegalArgumentException e) {
+                Prompt.imprimir(e.getMessage());
+            }
+        }
+    }
+
+    private String lerRg() {
+        while (true) {
+            String rg = Prompt.lerLinha("Informe o RG do funcionario: ");
+            try {
+                Funcionario funcionario = new Funcionario();
+                funcionario.setRg(rg);
+                return rg;
+            } catch (IllegalArgumentException e) {
+                Prompt.imprimir(e.getMessage());
+            }
+        }
+    }
+
+    private String lerCpf() {
+        while (true) {
+            String cpf = Prompt.lerLinha("Informe o CPF do funcionario (formato: 123.456.789-00): ");
+            try {
+                Funcionario funcionario = new Funcionario();
+                funcionario.setCpf(cpf);
+                return cpf;
+            } catch (IllegalArgumentException e) {
+                Prompt.imprimir(e.getMessage());
+            }
+        }
+    }
+
+    private String lerCep() {
+        while (true) {
+            String cep = Prompt.lerLinha("Informe o CEP do funcionario: ");
+            try {
+                Funcionario funcionario = new Funcionario();
+                funcionario.setCep(cep);
+                return cep;
+            } catch (IllegalArgumentException e) {
+                Prompt.imprimir(e.getMessage());
+            }
+        }
+    }
+
+    private String lerEndereco() {
+        while (true) {
+            String endereco = Prompt.lerLinha("Informe o endereço do funcionario: ");
+            try {
+                Funcionario funcionario = new Funcionario();
+                funcionario.setEndereco(endereco);
+                return endereco;
+            } catch (IllegalArgumentException e) {
+                Prompt.imprimir(e.getMessage());
+            }
+        }
+    }
+
+    private String lerBairro() {
+        while (true) {
+            String bairro = Prompt.lerLinha("Informe o bairro do funcionario: ");
+            try {
+                Funcionario funcionario = new Funcionario();
+                funcionario.setBairro(bairro);
+                return bairro;
+            } catch (IllegalArgumentException e) {
+                Prompt.imprimir(e.getMessage());
+            }
+        }
+    }
+
+    private String lerCidade() {
+        while (true) {
+            String cidade = Prompt.lerLinha("Informe a cidade do funcionario: ");
+            try {
+                Funcionario funcionario = new Funcionario();
+                funcionario.setCidade(cidade);
+                return cidade;
+            } catch (IllegalArgumentException e) {
+                Prompt.imprimir(e.getMessage());
+            }
+        }
+    }
+
+
+
+    public String getCpf() {
+        return lerCpf();
+    }
+
+    public Funcionario alterarFuncionario() {
+        String nomeFuncionario = lerNomeFuncionario();
+        String rg = lerRg();
+        String cpf = lerCpf();
+        String cep = lerCep();
+        String endereco = lerEndereco();
+        String bairro = lerBairro();
+        String cidade = lerCidade();
+
+        Funcionario funcionarioAlterado = new Funcionario(nomeFuncionario, rg, cpf, cep, endereco, bairro, cidade);
+        return funcionarioAlterado;
+    }
+
+}
